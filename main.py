ab69d8de51c4001ffca2def6251689937d433ae0
Chandrasekhar429857013 <63460964+Chandrasekhar429857013@users.noreply.github.com>
2021-03-14 19:19:25 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,89 @@
+# variable to represent number of protons
+nop = input("Please enter the number of protons - ")
+
+
+
+# Variable to represent number of neutrons
+non = input("Please enter the number of neutrons - ")
+
+atomic_mass = str(int(nop) + int(non)) + "g"
+
+if nop=='1' and non=='0':
+    print("The element is Hydrogen")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='2' and non=='2':
+    print("The element is Helium")
+    print("The atomic mass of this element is " + atomic_mass)
+    
+elif nop=='3' and non=='4':
+    print("The element is Lithium")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='4' and non=='5':
+    print("The element is Beryllium")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='5' and non=='6':
+    print("The element is Boron")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='6' and non=='6':
+    print("The element is Carbon")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='7' and non=='7':
+    print("The element is Nitrogen")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='8' and non=='8':
+    print("The element is Oxygen")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='9' and non=='10':
+    print("The element is Flourine")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='10' and non=='10':
+    print("The element is Neon")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='11' and non=='12':
+    print("The element is Sodium")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='12' and non=='12':
+    print("The element is Magnesium")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='13' and non=='14':
+    print("The element is Aluminium")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='14' and non=='14':
+    print("The element is Silicon")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='15' and non=='16':
+    print("The element is Phosphorus")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='16' and non=='16':
+    print("The element is Sulphur")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='17' and non=='18':
+    print("The element is Chlorine")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='18' and non=='22':
+    print("The element is Argon")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='19' and non=='20':
+    print("The element is Potassium")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='20' and non=='20':
+    print("The element is Calcium")
+    print("The atomic mass of this element is " + atomic_mass)
