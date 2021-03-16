eb5f066630387a7caf2fe783ca41b739b5279171
Chandrasekhar429857013 <63460964+Chandrasekhar429857013@users.noreply.github.com>
2021-03-16 08:44:06 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ nop = input("Please enter the number of protons - ")
 # Variable to represent number of neutrons
 non = input("Please enter the number of neutrons - ")
 
-atomic_mass = str(int(nop) + int(non)) + "g"
+atomic_mass = str(int(nop) + int(non)) + "u"
 
+#All if/else if loops to give output
 if nop=='1' and non=='0':
     print("The element is Hydrogen")
     print("The atomic mass of this element is " + atomic_mass)
@@ -86,4 +87,84 @@ elif nop=='19' and non=='20':
 
 elif nop=='20' and non=='20':
     print("The element is Calcium")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='21' and non=='24':
+    print("The element is Sacndium")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='22' and non=='26':
+    print("The element is Titanium")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='23' and non=='28':
+    print("The element is Vanaium")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='24' and non=='28':
+    print("The element is Chromium")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='25' and non=='30':
+    print("The element is Manganeese")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='26' and non=='30':
+    print("The element is Iron")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='27' and non=='32':
+    print("The element is Cobalt")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='28' and non=='31':
+    print("The element is Nickel")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='29' and non=='32':
+    print("The element is Copper")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='30' and non=='35':
+    print("The element is Zinc")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='31' and non=='39':
+    print("The element is Gallium")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='32' and non=='41':
+    print("The element is Germanium")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='33' and non=='42':
+    print("The element is Arsenic")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='34' and non=='45':
+    print("The element is Selenium")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='35' and non=='45':
+    print("The element is Bromine")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='36' and non=='48':
+    print("The element is Krypton")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='37' and non=='48':
+    print("The element is Rubidium")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='38' and non=='50':
+    print("The element is Strontium")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='39' and non=='50':
+    print("The element is Yttrium")
+    print("The atomic mass of this element is " + atomic_mass)
+
+elif nop=='40' and non=='51':
+    print("The element is Zirconium")
     print("The atomic mass of this element is " + atomic_mass)
